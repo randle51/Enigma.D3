38a10a81b2740c944feb4ff912ec26c69b279d93
enigma32 <enigma32@outlook.com>
2015-06-13 22:28:31 +0000

    - Debug option added to maphack. Covers the minimap control with blue to see that overlay rendering is working.

--- Enigma.D3.MapHack/D3/MapHack/Markers/MapMarkerOptions.cs
+++ Enigma.D3.MapHack/D3/MapHack/Markers/MapMarkerOptions.cs
@@ -12,10 +12,12 @@ namespace Enigma.D3.MapHack.Markers
 
 		public static MapMarkerOptions Instance { get { return _lazyInstance.Value; } }
 
+		private bool _debugVisibility = false;
 		private bool _showWreckables = false;
 		private bool _showChests = true;
 		private bool _showMonsters = true;
 
+		public bool DebugVisibility { get { return _debugVisibility; } set { if (_debugVisibility != value) { _debugVisibility = value; Refresh("DebugVisibility"); } } }
 		public bool ShowWreckables { get { return _showWreckables; } set { if (_showWreckables != value) { _showWreckables = value; Refresh("ShowWreckables"); } } }
 		public bool ShowChests { get { return _showChests; } set { if (_showChests != value) { _showChests = value; Refresh("ShowChests"); } } }
 		public bool ShowMonsters { get { return _showMonsters; } set { if (_showMonsters != value) { _showMonsters = value; Refresh("ShowMonsters"); } } }
--- Enigma.D3.MapHack/D3/MapHack/Minimap.cs
+++ Enigma.D3.MapHack/D3/MapHack/Minimap.cs
@@ -26,6 +26,8 @@ namespace Enigma.D3.MapHack
 
 		public Minimap(Canvas overlay)
 		{
+			MapMarkerOptions.Instance.PropertyChanged += (s, e) => Refresh();
+
 			_minimapItems = new ObservableCollection<IMapMarker>();
 
 			_root = new Canvas() { Height = 1200.5d };
@@ -37,6 +39,8 @@ namespace Enigma.D3.MapHack
 
 			UpdateSizeAndPosition();
 		}
+
+		public bool DebugVisibility { get { return MapMarkerOptions.Instance.DebugVisibility; } }
 
 		public ObservableCollection<IMapMarker> MinimapMarkers { get { return _minimapItems; } }
 
