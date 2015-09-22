763eeff163265dd13ee0c3069c62af83cc13b756
enigma32 <enigma32@outlook.com>
2015-09-22 21:25:01 +0000

    Updated for 2.3.0.33567 NOTE: The "API" is unstable as things are being rewritten.

--- Enigma.D3.Memory/D3/Engine.cs
+++ Enigma.D3.Memory/D3/Engine.cs
@@ -19,37 +19,37 @@ namespace Enigma.D3
 		private static class Addr
 		{
 			public const int SnoGroupInitializers = 0x01B71ABC - 4;
-			public const int SnoGroupByCode = 0x01DCD028;
-			public const int SnoGroups = 0x01DCD144;
-			public const int SnoGroupSearch = 0x01E201FC;
-			public const int SnoFilesAsync = 0x01E20200;
-			public const int ObjectManager = 0x01DCF22C;
-			public const int ObjectManagerPristine = 0x01DCF230;
-			public const int MessageDescriptor = 0x01E8384C;
-			public const int MapActId = 0x01BBB328;
-			public const int LocalData = 0x01DD04D0;
-			public const int LevelArea = 0x01D27758;
-			public const int LevelAreaName = 0x01D27788;
-			public const int GameplayPreferences = 0x01BA1F74;
-			public const int ContainerManager = 0x01E8454C;
-			public const int BuffManager = 0x01DB4970;
-			public const int ApplicationLoopCount = 0x01DCF2A0;
+			public const int SnoGroupByCode = 0x01DCD048;
+			public const int SnoGroups = 0x01DCD164;
+			public const int SnoGroupSearch = 0x01E2021C;
+			public const int SnoFilesAsync = 0x01E20220;
+			public const int ObjectManager = 0x01DCF24C;
+			public const int ObjectManagerPristine = 0x01DCF250;
+			public const int MessageDescriptor = 0x01E8386C;
+			public const int MapActId = 0x01BBB348;
+			public const int LocalData = 0x01DD04F0;
+			public const int LevelArea = 0x01D27778;
+			public const int LevelAreaName = 0x01D277A8;
+			public const int GameplayPreferences = 0x01BA1F94;
+			public const int ContainerManager = 0x01E8456C;
+			public const int BuffManager = 0x01DB4990;
+			public const int ApplicationLoopCount = 0x01DCF2C0;
 			public const int AttributeDescriptors = 0x01B76AC8;
-			public const int VideoPreferences = 0x01BA1A30;
-			public const int ChatPreferences = 0x01BA2004;
-			public const int SoundPreferences = 0x01BA1AC4;
-			public const int SocialPreferences = 0x01BA1FD4;
+			public const int VideoPreferences = 0x01BA1A50;
+			public const int ChatPreferences = 0x01BA2024;
+			public const int SoundPreferences = 0x01BA1AE4;
+			public const int SocialPreferences = 0x01BA1FF4;
 			public const int UIHandlers = 0x01B684D0;
-			public const int UIReferences = 0x01BBB8D8;
-			public const int SnoIdToEntityId = 0x01DE199C;
-			public const int TrickleManager = 0x01D8BF68;
-			public const int PtrSnoFiles = 0x01DD15F0;
+			public const int UIReferences = 0x01BBB8F8;
+			public const int SnoIdToEntityId = 0x00000000;
+			public const int TrickleManager = 0x01D8BF88;
+			public const int PtrSnoFiles = 0x01DD1610;
 		}
 
 		private static class Const
 		{
 			public const int SizeOfAttributeDescriptor = 40;
-			public const int CountAttributeDescriptors = (0x01B84874 - 0x01B76AE4) / SizeOfAttributeDescriptor;
+			public const int CountAttributeDescriptors = (0x01B8489C - 0x01B76AE4) / SizeOfAttributeDescriptor;
 
 			public const int CountUIReferences = 2767;
 
@@ -63,7 +63,7 @@ namespace Enigma.D3
 		[ThreadStatic]
 		private static Engine _current;
 
-		public static readonly Version SupportedVersion = new Version(2, 3, 0, 33414);
+		public static readonly Version SupportedVersion = new Version(2, 3, 0, 33567);
 
 		public static Engine Create()
 		{
