86a2d97c5dbf962c9c6fca73f03135b7492780d4
enigma32 <enigma32@5757b266-7caa-4267-910b-59fad70909ba>
2016-08-13 00:32:07 +0000

    New application added: Enigma.D3.Explorer, a "Live Watch" window.

---
+++ Enigma.D3.Explorer/Utilities/MemoryObjectClassGeneratorDialog.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Enigma.D3.Explorer.Utilities
+{
+	/// <summary>
+	/// Interaction logic for MemoryObjectClassGeneratorDialog.xaml
+	/// </summary>
+	public partial class MemoryObjectClassGeneratorDialog : Window
+	{
+		public string HeaderText
+		{
+			get { return (string)GetValue(HeaderTextProperty); }
+			set { SetValue(HeaderTextProperty, value); }
+		}
+
+		// Using a DependencyProperty as the backing store for HeaderText.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty HeaderTextProperty =
+			DependencyProperty.Register("HeaderText", typeof(string), typeof(MemoryObjectClassGeneratorDialog), new PropertyMetadata(""));
+
+		public string StatusText
+		{
+			get { return (string)GetValue(StatusTextProperty); }
+			set { SetValue(StatusTextProperty, value); }
+		}
+
+		// Using a DependencyProperty as the backing store for StatusText.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty StatusTextProperty =
+			DependencyProperty.Register("StatusText", typeof(string), typeof(MemoryObjectClassGeneratorDialog), new PropertyMetadata(""));
+
+		public double ProgressPercentage
+		{
+			get { return (double)GetValue(ProgressPercentageProperty); }
+			set { SetValue(ProgressPercentageProperty, value); }
+		}
+
+		// Using a DependencyProperty as the backing store for ProgressPercent.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty ProgressPercentageProperty =
+			DependencyProperty.Register("ProgressPercentage", typeof(double), typeof(MemoryObjectClassGeneratorDialog), new PropertyMetadata(0.0d));
+
+		public MemoryObjectClassGeneratorDialog()
+		{
+			InitializeComponent();
+			LayoutRoot.DataContext = this;
+		}
+
+		private void OnCancelClicked(object sender, RoutedEventArgs e)
+		{
+			DialogResult = false;
+		}
+	}
+}
---
+++ Enigma.D3.Explorer/Views/CustomContentView.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Enigma.D3.Explorer.Views
+{
+	internal abstract class CustomContentView<T> : PollView
+	{
+		private bool _creationRequired = true;
+		protected T Context { get; private set; }
+		protected ConcurrentBag<Action> Refreshers { get; private set; }
+
+		public CustomContentView()
+		{
+			Refreshers = new ConcurrentBag<Action>();
+		}
+
+		public override void Enter()
+		{
+			base.Enter();
+			try
+			{
+				CreateContentIfRequired();
+			}
+			catch (Exception exception)
+			{
+				Content = new TextBlock { Text = exception.ToString() };
+			}
+		}
+
+		public override void Refresh()
+		{
+			base.Refresh();
+			if (!CreateContentIfRequired())
+			{
+				Context = CreateContext();
+				foreach (var refresher in Refreshers)
+					refresher.Invoke();
+			}
+		}
+
+		private bool CreateContentIfRequired()
+		{
+			var engine = GetEngine();
+			if (_creationRequired)
+			{
+				Context = CreateContext();
+				if (Context == null)
+					return false;
+				Content = CreateContent();
+				_creationRequired = false;
+				return true;
+			}
+			return false;
+		}
+
+		protected abstract object CreateContent();
+
+		protected abstract T CreateContext();
+
+		protected TValue Dynamic<TValue>(TValue value, Action<TValue> action)
+		{
+			Refreshers.Add(() => action(value));
+			return Using(value, action);
+		}
+
+		protected TValue Using<TValue>(TValue value, Action<TValue> action)
+		{
+			action(value);
+			return value;
+		}
+	}
+}
