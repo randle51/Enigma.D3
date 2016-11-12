6ff69a2d5e54a779fd3a4c5ac2f22c0d2e0431b5
Enigma32 <enigma32@outlook.com>
2016-11-12 18:30:57 +0100

    Update memory offsets for build 41394

--- Enigma.D3.Memory.Generated/MethodPtr.cs
+++ Enigma.D3.Memory.Generated/MethodPtr.cs
@@ -2,7 +2,7 @@ namespace Enigma.D3.Memory
 {
 	public static class MethodPtr
 	{
-		public const int SNOGroups_Initialize = 0x0107A230;
-		public const int GameMessage_GetHandlerInfo = 0x012C7DB0;
+		public const int SNOGroups_Initialize = 0x0107A290;
+		public const int GameMessage_GetHandlerInfo = 0x012C7E10;
 	}
 }
