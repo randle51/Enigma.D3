b1eb9aa25801a7170ce774c153ff95fa970f40f8
Enigma32 <enigma32@outlook.com>
2017-01-19 19:55:15 +0100

    Update memory offsets for build 43048

--- Enigma.D3.Memory.Generated/MethodPtr.cs
+++ Enigma.D3.Memory.Generated/MethodPtr.cs
@@ -2,7 +2,7 @@ namespace Enigma.D3.Memory
 {
 	public static class MethodPtr
 	{
-		public const int SNOGroups_Initialize = 0x00FC91F0;
-		public const int GameMessage_GetHandlerInfo = 0x011DC220;
+		public const int SNOGroups_Initialize = 0x00FC85E0;
+		public const int GameMessage_GetHandlerInfo = 0x011DB5F0;
 	}
 }
--- Enigma.D3.Memory.Generated/ObjectPtr.cs
+++ Enigma.D3.Memory.Generated/ObjectPtr.cs
@@ -2,23 +2,23 @@ namespace Enigma.D3.Memory
 {
 	public static class ObjectPtr
 	{
-		public const int VideoPreferences = 0x019CE0A0;
-		public const int SoundPreferences = 0x019CE140;
-		public const int HotkeyPreferences = 0x019CE190;
-		public const int GameplayPreferences = 0x019CE610;
-		public const int SocialPreferences = 0x019CE674;
-		public const int ChatPreferences = 0x019CE6A4;
-		public const int LevelArea = 0x01B9E68C;
-		public const int LevelAreaName = 0x01B9E6BC;
-		public const int MapActId = 0x01B9EA00;
-		public const int SNOGroups = 0x01C13C8C;
-		public const int SNOGroupsByCode = 0x01C13C90;
-		public const int ObjectManager = 0x01C13E98;
-		public const int ObjectManagerPristine = 0x01C13E9C;
-		public const int ApplicationLoopCount = 0x01C13F14;
-		public const int LocalData = 0x01C15148;
-		public const int AttributeDescriptors = 0x01C66660;
-		public const int ContainerManager = 0x01CD6A7C;
-		public const int MessageDescriptor = 0x01CD6A80;
+		public const int VideoPreferences = 0x019CD0A0;
+		public const int SoundPreferences = 0x019CD140;
+		public const int HotkeyPreferences = 0x019CD190;
+		public const int GameplayPreferences = 0x019CD610;
+		public const int SocialPreferences = 0x019CD674;
+		public const int ChatPreferences = 0x019CD6A4;
+		public const int LevelArea = 0x01B9D68C;
+		public const int LevelAreaName = 0x01B9D6BC;
+		public const int MapActId = 0x01B9DA00;
+		public const int SNOGroups = 0x01C12C8C;
+		public const int SNOGroupsByCode = 0x01C12C90;
+		public const int ObjectManager = 0x01C12E98;
+		public const int ObjectManagerPristine = 0x01C12E9C;
+		public const int ApplicationLoopCount = 0x01C12F14;
+		public const int LocalData = 0x01C14148;
+		public const int AttributeDescriptors = 0x01C65660;
+		public const int ContainerManager = 0x01CD5A7C;
+		public const int MessageDescriptor = 0x01CD5A80;
 	}
 }
