da07a6dc63714f3491f78fef4e68012d7860d940
Enigma32 <enigma32@outlook.com>
2017-03-26 18:12:25 +0200

    Update memory-gen output

--- Enigma.D3.Memory.Generated/MethodPtr.cs
+++
@@ -1,8 +0,0 @@
-namespace Enigma.D3.Memory
-{
-	public static class MethodPtr
-	{
-		public const int SNOGroups_Initialize = 0x012631A0;
-		public const int GameMessage_GetHandlerInfo = 0x01487D80;
-	}
-}
