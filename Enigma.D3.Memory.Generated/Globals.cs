82b6aae68e38dc08951ce3c4b2c85d99653b8993
Enigma32 <enigma32@outlook.com>
2017-06-28 17:47:53 +0200

    Update Memory constants for 2.6.0.46006

--- Enigma.D3.Memory.Generated/Globals.cs
+++ Enigma.D3.Memory.Generated/Globals.cs
@@ -4,11 +4,11 @@ namespace Enigma.D3.Memory
 {
 	public static class Globals
 	{
-		public static readonly Version SupportedVersion = new Version(2, 5, 0, 44247);
+		public static readonly Version SupportedVersion = new Version(2, 6, 0, 46006);
 		public const int SNOGroupsCount = 60;
-		public const int AttributeDescriptorsCount = 1445;
-		public const int SizeOf_PlayerData = 0xC0E0;
-		public const int Offset_PlayerData_HeroName = 0xA1D0;
-		public const int Offset_PlayerData_LifePercentage = 0xB154;
+		public const int AttributeDescriptorsCount = 0x5B9;
+		public const int SizeOf_PlayerData = 0xD158;
+		public const int Offset_PlayerData_HeroName = 0xB1C0;
+		public const int Offset_PlayerData_LifePercentage = 0xC1BC;
 	}
 }
--- Enigma.D3.Memory.Generated/ObjectPtr.cs
+++ Enigma.D3.Memory.Generated/ObjectPtr.cs
@@ -2,23 +2,23 @@ namespace Enigma.D3.Memory
 {
     public static class ObjectPtr
     {
-        public const int VideoPreferences = 0x01E0F438;
-        public const int SoundPreferences = 0x01E0F4D8;
-        public const int HotkeyPreferences = 0x01E0F528;
-        public const int GameplayPreferences = 0x01E0F9A8;
-        public const int SocialPreferences = 0x01E0FA10;
-        public const int ChatPreferences = 0x01E0FA40;
-        public const int LevelArea = 0x01FEABE0;
-        public const int LevelAreaName = 0x01FEABE8;
-        public const int MapActId = 0x01FEAED0;
-        public const int SNOGroupsByCode = 0x02059EA0;
-        public const int SNOGroups = 0x02059FB8;
-        public const int ObjectManager = 0x0205C0F0;
-        public const int ObjectManagerPristine = 0x0205C0F4;
-        public const int ApplicationLoopCount = 0x0205C16C;
-        public const int LocalData = 0x0205D3A0;
-        public const int AttributeDescriptors = 0x020B3A60;
-        public const int ContainerManager = 0x02124644;
-        public const int MessageDescriptor = 0x02124650;
+        public const int VideoPreferences = 0x1EECE70;
+        public const int SoundPreferences = 0x1EECF10;
+        public const int HotkeyPreferences = 0x1EECF60;
+        public const int GameplayPreferences = 0x1EED3E0;
+        public const int SocialPreferences = 0x1EED448;
+        public const int ChatPreferences = 0x1EED478;
+        public const int LevelArea = 0x20D5150;
+        public const int LevelAreaName = 0x20D5158;
+        public const int MapActId = 0x20D5440;
+        public const int SNOGroupsByCode = 0x2144838;
+        public const int SNOGroups = 0x2144834;
+        public const int ObjectManager = 0x2146A80;
+        public const int ObjectManagerPristine = 0x2146A80;
+        public const int ApplicationLoopCount = 0x2146AFC;
+        public const int LocalData = 0x2147D30;
+        public const int AttributeDescriptors = 0x219EDE8;
+        public const int ContainerManager = 0x2211824;
+        public const int MessageDescriptor = 0x2211830;
     }
 }
