2f225135595216f9817809d66ef1708b4f6db070
Enigma32 <enigma32@outlook.com>
2017-07-10 01:13:46 +0200

    Simplify 32-bit/64-bit offset selection

---
+++ Enigma.D3.MemoryModel/MemoryObjectExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Enigma.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma.D3.MemoryModel
+{
+    public static class MemoryObjectExtensions
+    {
+        public static T PlatformRead<T>(this MemoryObject memoryObject, int x86, int x64)
+        {
+            var offset = SymbolTable.Current.Platform == Platform.X86 ? x86 : x64;
+            return memoryObject.Read<T>(offset);
+        }
+    }
+}
