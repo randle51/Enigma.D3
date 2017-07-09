0b9c97f42ff01fbc5c38aeedf560eb5d3440ac26
Enigma32 <enigma32@outlook.com>
2017-07-10 01:28:48 +0200

    Add GameDifficulty enum

---
+++ Enigma.D3.Core/Enums/GameDifficulty.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma.D3.Enums
+{
+    public enum GameDifficulty : int
+    {
+        Normal = 0,
+        Hard,
+        Expert,
+        Master,
+        Torment1,
+        Torment2,
+        Torment3,
+        Torment4,
+        Torment5,
+        Torment6,
+        Torment7,
+        Torment8,
+        Torment9,
+        Torment10,
+        Torment11,
+        Torment12,
+        Torment13,
+    }
+}
