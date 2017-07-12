7cae5ea6eea3d7d569d5a5033396ea4508bb3789
Enigma32 <enigma32@outlook.com>
2017-07-12 20:09:23 +0200

    Add dictionary and general getters

--- Enigma.D3.MemoryModel/Core/AttributeReader.cs
+++ Enigma.D3.MemoryModel/Core/AttributeReader.cs
@@ -30,5 +30,35 @@ namespace Enigma.D3.MemoryModel.Core
             value = default(AttributeValue);
             return false;
         }
+
+        public double GetAttributeValue(int groupId, AttributeId attributeId, int modifier = -1)
+        {
+            AttributeValue value;
+            var descriptor = MemoryContext.Current.DataSegment.AttributeDescriptors[(int)attributeId];
+            if (!TryGetAttributeValue(groupId, attributeId, modifier, out value))
+                value = new AttributeValue { Int32 = descriptor.DefaultValue };
+            return descriptor.DataType == typeof(int) ? value.Int32 : value.Single;
+        }
+
+        public Dictionary<AttributeKey, double> GetAttributes(int groupId)
+        {
+            var group = MemoryContext.Current.DataSegment.ObjectManager.FastAttrib.FastAttribGroups[groupId];
+
+            var values = new Dictionary<AttributeKey, double>();
+
+            foreach (var map in new[] { group.PtrMap.Dereference(), group.Map })
+            {
+                if (map == null)
+                    continue;
+
+                foreach (var kvp in map)
+                {
+                    var descriptor = MemoryContext.Current.DataSegment.AttributeDescriptors[(int)kvp.Key.Id];
+                    values[kvp.Key] = descriptor.DataType == typeof(int) ? kvp.Value.Int32 : kvp.Value.Single;
+                }
+            }
+
+            return values;
+        }
     }
 }
