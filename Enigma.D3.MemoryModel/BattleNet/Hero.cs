331676d674a462349a7c6e3dc54f3614d19ce31a
Enigma32 <enigma32@outlook.com>
2017-07-14 21:11:57 +0200

    Add 64-bit offsets to Hero structure

--- Enigma.D3.MemoryModel/BattleNet/Hero.cs
+++ Enigma.D3.MemoryModel/BattleNet/Hero.cs
@@ -1,4 +1,6 @@
-﻿using Enigma.D3.Enums;
+﻿using Enigma.D3.DataTypes;
+using Enigma.D3.Enums;
+using Enigma.D3.MemoryModel;
 using Enigma.Memory;
 using System;
 using System.Collections.Generic;
@@ -6,27 +8,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Enigma.D3.MemoryModel32.BattleNet
+namespace Enigma.D3.MemoryModel.BattleNet
 {
-    // TODO: 64-bit, non-PTR
     public class Hero : MemoryObject
     {
-        public string Name
-            => ReadStringPointer(0xC4, 512).Dereference();
-
-        public ulong HeroId => Read<ulong>(0x0B8);
-        public int CurrentActivity => Read<int>(0x0D0);
-        public HeroClass Class => Read<HeroClass>(0x0D4);
-        public int Level => Read<int>(0x0DC);
-        public int Paragon => Read<int>(0x0E0);
-        public int HighestSoloRift => Read<int>(0x0E8);
-        public int PvpRank => Read<int>(0x0EC);
-        public int HighestAct => Read<int>(0x0F4);
-        public int Flags => Read<int>(0x124);
-        public int LastPlayedAct => Read<int>(0x130);
-        public int Quest => Read<int>(0x134);
-        public int QuestStep => Read<int>(0x138);
-        public TimeSpan Played => TimeSpan.FromSeconds(Read<int>(0x13C));
+        //public static int SizeOf => SymbolTable.Current.Platform == Platform.X86 ?
+        //    0 : 0x1A8;
+        
+        public ulong HeroId => this.PlatformRead<ulong>(0xB8, 0xC0);
+        public string Name => this.PlatformReadStringPointer(0xC4, 0xD0, maxLength: 512).Dereference();
+        public HeroClass Class => this.PlatformRead<HeroClass>(0xD4, 0xE8);
+        public int Level => this.PlatformRead<int>(0xDC, 0xF0);
+        public int Paragon => this.PlatformRead<int>(0xE0, 0xF4);
+        public int HighestSoloRift => this.PlatformRead<int>(0xE8, 0xFC);
+        public int PvpRank => this.PlatformRead<int>(0xEC, 0x100);
+        public int HighestAct => this.PlatformRead<int>(0xF4, 0x108);
+        public int Flags => this.PlatformRead<int>(0x124, 0x138);
+        public int LastPlayedAct => this.PlatformRead<int>(0x130, 0x14C);
+        public SNO Quest => this.PlatformRead<SNO>(0x134, 0x150);
+        public int QuestStep => this.PlatformRead<int>(0x138, 0x154);
+        public TimeSpan Played => TimeSpan.FromSeconds(this.PlatformRead<int>(0x13C, 0x158));
 
         public bool IsHardcore => (Flags & 1) != 0;
         public bool IsDeceased => (Flags & 8) != 0;
--- Enigma.D3.MemoryModel/BattleNet/Map.cs
+++ Enigma.D3.MemoryModel/BattleNet/Map.cs
@@ -5,18 +5,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
+using Enigma.D3.MemoryModel;
 
-namespace Enigma.D3.MemoryModel32.BattleNet
+namespace Enigma.D3.MemoryModel.BattleNet
 {
-    // TODO: 64-bit, non-PTR
     public class Map<T> : MemoryObject, IEnumerable<T>
     {
-        public const int SizeOf = 0x10;
+        public static int SizeOf => SymbolTable.Current.Platform == Platform.X86 ? 0x10 : 0x20;
 
         public int NumberOfSlots => Read<int>(0x00);
-        public Ptr<Ptr<Bucket>> Buckets => Read<Ptr<Ptr<Bucket>>>(0x04);
-        public int Count => Read<int>(0x08);
-        public float LoadFactor => Read<float>(0x0C);
+        public Ptr<Ptr<Bucket>> Buckets => this.PlatformRead<Ptr<Ptr<Bucket>>>(0x04, 0x08);
+        public int Count => this.PlatformRead<int>(0x08, 0x10);
+        public float LoadFactor => this.PlatformRead<float>(0x0C, 0x18);
 
         public IEnumerator<T> GetEnumerator()
         {
@@ -37,12 +37,14 @@ namespace Enigma.D3.MemoryModel32.BattleNet
 
         public class Bucket : MemoryObject
         {
-            public static int SizeOf = 0x10 + TypeHelper.SizeOf(typeof(T));
+            public static int SizeOf => SymbolTable.Current.Platform == Platform.X86 ?
+                0x10 + TypeHelper.SizeOf(typeof(T)) :
+                0x18 + TypeHelper.SizeOf(typeof(T));
 
             public Ptr<Bucket> Next => Read<Ptr<Bucket>>(0x00);
-            public Ptr<Bucket> Prev => Read<Ptr<Bucket>>(0x04);
-            public ulong Hash => Read<ulong>(0x08);
-            public T Value => Read<T>(0x10);
+            public Ptr<Bucket> Prev => this.PlatformRead<Ptr<Bucket>>(0x04, 0x08);
+            public ulong Hash => this.PlatformRead<ulong>(0x08, 0x10);
+            public T Value => this.PlatformRead<T>(0x10, 0x18);
         }
     }
 }
