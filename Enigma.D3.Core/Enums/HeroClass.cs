569e6e10104e397f499a79cef6a9abfe6b38804f
Enigma32 <enigma32@outlook.com>
2017-07-15 08:35:25 +0200

    Add Necromancer

--- Enigma.D3.Core/Enums/HeroClass.cs
+++ Enigma.D3.Core/Enums/HeroClass.cs
@@ -5,14 +5,15 @@ using System.Text;
 
 namespace Enigma.D3.Enums
 {
-	public enum HeroClass
-	{
-		None = -1,
-		DemonHunter = 0,
-		Barbarian = 1,
-		Wizard = 2,
-		Witchdoctor = 3,
-		Monk = 4,
-		Crusader = 5
-	}
+    public enum HeroClass
+    {
+        None = -1,
+        DemonHunter = 0,
+        Barbarian = 1,
+        Wizard = 2,
+        Witchdoctor = 3,
+        Monk = 4,
+        Crusader = 5,
+        Necromancer = 6
+    }
 }
