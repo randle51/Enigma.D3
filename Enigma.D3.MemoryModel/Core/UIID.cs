aa9cc62290e103244d3de9f73a86e828ff27d6c7
Enigma32 <enigma32@outlook.com>
2017-07-22 15:40:19 +0200

    Add implicit cast from UIID to UIKey

--- Enigma.D3.MemoryModel/Core/UIID.cs
+++ Enigma.D3.MemoryModel/Core/UIID.cs
@@ -11,6 +11,8 @@ namespace Enigma.D3.MemoryModel.Core
     {
         public const int SizeOf = 0x208;
 
+        public static implicit operator UIKey(UIID id) => id.Key;
+
         public ulong Key => Read<ulong>(0x000);
         public string Name => ReadString(0x008, 0x200);
 
