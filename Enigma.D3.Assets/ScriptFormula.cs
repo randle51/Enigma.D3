bd5ee9a3dab3b2b7105b8fe7db04b404288e997c
Enigma32 <enigma32@outlook.com>
2017-07-26 17:21:43 +0200

    Add TagMap and ScriptFormula

---
+++ Enigma.D3.Assets/ScriptFormula.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Enigma.Memory;
+
+namespace Enigma.D3.Assets
+{
+    public class ScriptFormula
+    {
+        public int x00;
+        public int x04;
+        public int x08;
+        public int x0C;
+        public int x10;
+        public int OpCodeNameSize;
+        public int x18;
+        public int OpCodeSize;
+        public string OpCodeName;
+        public byte[] OpCode;
+
+        public ScriptFormula(BufferMemoryReader reader, ref int offset)
+        {
+            x00 = reader.Read<int>(offset + 0x00);
+            x04 = reader.Read<int>(offset + 0x04);
+            x08 = reader.Read<int>(offset + 0x08);
+            x0C = reader.Read<int>(offset + 0x0C);
+            x10 = reader.Read<int>(offset + 0x10);
+            OpCodeNameSize = reader.Read<int>(offset + 0x14);
+            x18 = reader.Read<int>(offset + 0x18);
+            OpCodeSize = reader.Read<int>(offset + 0x1C);
+            OpCodeName = reader.ReadString(offset + 0x20, OpCodeNameSize);
+            OpCode = reader.ReadBytes(offset + 0x20 + MemoryObject.AlignedSize(OpCodeNameSize, 4), OpCodeSize);
+            offset += 0x20 + MemoryObject.AlignedSize(OpCodeNameSize, 4) + MemoryObject.AlignedSize(OpCodeSize, 4);
+        }
+    }
+}
---
+++ Enigma.D3.Assets/TagMap.cs
@@ -0,0 +1,82 @@
+﻿using Enigma.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using Enigma.D3.DataTypes;
+using Enigma.D3.Enums;
+
+namespace Enigma.D3.Assets
+{
+    public class TagMap
+    {
+        public TagMap(byte[] data)
+        {
+            if (data == null)
+                throw new ArgumentNullException(nameof(data));
+
+            Reader = new BufferMemoryReader(data);
+            if (data.Length > 0)
+            {
+                Count = Reader.Read<int>(0x00);
+                Entries = new Entry[Count];
+
+                var offset = 0x04;
+                for (int i = 0; i < Count; i++)
+                {
+                    Entries[i] = new Entry();
+                    Entries[i].Type = Reader.Read<int>(offset); offset += 4;
+                    Entries[i].Key = Reader.Read<int>(offset); offset += 4;
+                    switch (Entries[i].Type)
+                    {
+                        case 0:
+                            Entries[i].Value = Reader.Read<int>(offset); offset += 4;
+                            break;
+
+                        case 1:
+                            Entries[i].Value = Reader.Read<float>(offset); offset += 4;
+                            break;
+
+                        case 2:
+                            Entries[i].Value = Reader.Read<SNO>(offset); offset += 4;
+                            break;
+
+                        case 3:
+                            Entries[i].Value = Reader.Read<GBID>(offset); offset += 4;
+                            break;
+
+                        case 4:
+                            Entries[i].Value = new ScriptFormula(Reader, ref offset);
+                            break;
+
+                        case 6:
+                            Entries[i].Value = Reader.Read<AttributeId>(offset); offset += 4;
+                            break;
+
+                        default:
+                            throw new NotImplementedException($"Entry type {Entries[i].Type} deserialization not implemented.");
+                    }
+                }
+            }
+        }
+
+        protected BufferMemoryReader Reader { get; }
+
+        public int Count { get; }
+        public Entry[] Entries { get; }
+
+        public class Entry
+        {
+            public int Type { get; internal set; }
+            public int Key { get; internal set; }
+            public object Value { get; internal set; }
+
+            public override string ToString()
+            {
+                return $"[{Key}]({Value.GetType().Name}): {Value}";
+            }
+        }
+    }
+}
