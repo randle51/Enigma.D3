5855ab11acc72c9d80ada1e7a174128cb0af5272
Enigma32 <enigma32@outlook.com>
2017-07-28 17:56:20 +0200

    Add container cache extensions

---
+++ Enigma.D3.MemoryModel.Caching/ContainerExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Enigma.D3.MemoryModel.Collections;
+using Enigma.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma.D3.MemoryModel.Caching
+{
+    public static class ContainerExtensions
+    {
+        public static List<T> GetCachedItems<T>(this Container<T> container) where T : MemoryObject
+        {
+            var cache = new ContainerCache<T>(container);
+            cache.Update();
+            return cache.NewItems;
+        }
+    }
+}
